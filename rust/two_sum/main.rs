b4a6875468936f62f2e92aabdba3f897ef1998da
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-10-13 15:36:15 -0300

    refactor add dir structure

--- rust/two_sum/main.rs
+++ rust/two_sum/main.rs
@@ -1,16 +1,16 @@
 fn main(){
     let nums = vec![2, 7, 11, 15];
     let target = 9;
-    let result = two_sum(&nums, target);
+    let result = two_sum(nums, target);
     println!("The indexs are: {:?}", result);
 
-     let nums2 = vec![3, 2, 4];
+    let nums2 = vec![3, 2, 4];
     let target2 = 6;
-    let result2 = two_sum(&nums2, target2);
+    let result2 = two_sum(nums2, target2);
     println!("The indexs are: {:?}", result2);
 }
 
-fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = std::collections::HashMap::new();
 
     for (i, &num) in nums.iter().enumerate() {
