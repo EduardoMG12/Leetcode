022e3087d223b22b37f286196adec98abdea038f
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-10-13 14:37:55 -0300

    feat: make first exercise two sum

---
+++ rust/two_sum.rs
@@ -0,0 +1,25 @@
+fn main(){
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    let result = two_sum(&nums, target);
+    println!("The indexs are: {:?}", result);
+
+     let nums2 = vec![3, 2, 4];
+    let target2 = 6;
+    let result2 = two_sum(&nums2, target2);
+    println!("The indexs are: {:?}", result2);
+}
+
+fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = std::collections::HashMap::new();
+
+    for (i, &num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if let Some(&index) = map.get(&complement) {
+            return vec![index as i32, i as i32];
+        }
+        map.insert(num, i);
+    }
+
+    vec![]
+}
